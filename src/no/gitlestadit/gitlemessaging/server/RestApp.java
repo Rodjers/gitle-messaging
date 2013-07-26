2900fc20ff77d9502f770737bcf5faa02c1dd1ad
Oddgeir Gitlestad <oddgeir.gitlestad@gitlestadit.no>
2013-07-26 10:35:01 +0200

    Minor code cleanup

--- src/no/gitlestadit/gitlemessaging/server/RestApp.java
+++ src/no/gitlestadit/gitlemessaging/server/RestApp.java
@@ -49,7 +49,7 @@ public class RestApp extends HttpServlet {
         
 	}
 	
-	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+	public void doPut(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
 		
 		String name = req.getParameter("name");
@@ -62,34 +62,34 @@ public class RestApp extends HttpServlet {
    		resp.getWriter().println(app.getJSONString());
     	}
 	
-//	public void doPost(HttpServletRequest req, HttpServletResponse resp)
-//            throws IOException {
-//		
-//		StringBuffer url = req.getRequestURL();
-//		
-//		String appKey = url.substring(url.lastIndexOf("/")+1);
-//		String gcmSenderId = req.getParameter("gcmSenderId");
-//		String gcmSenderSecret = req.getParameter("gcmSenderSecret");
-//		String apnSenderId = req.getParameter("apnSenderId");
-//		String apnSenderSecret = req.getParameter("apnSenderSecret");
-//		String wnsSenderId = req.getParameter("wnsSenderId");
-//		String wnsSenderSecret = req.getParameter("wnsSenderSecret");
-//   		
-//		DatabaseHandle db = new DatabaseHandle();
-//		
-//		App app = db.getApp(appKey);
-//		
-//		app.setApnSenderSecret(apnSenderSecret);
-//		app.setApnSenderId(apnSenderId);
-//		app.setGcmSenderId(gcmSenderId);
-//		app.setGcmSenderSecret(gcmSenderSecret);
-//		app.setWnsSenderId(wnsSenderId);
-//		app.setWnsSenderSecret(wnsSenderSecret);		
-//		
-//		db.updateApp(app);
-//		
-//   		resp.getWriter().println(req.getParameter(app.getJSONString()));
-//    	}
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+		
+		StringBuffer url = req.getRequestURL();
+		
+		String appKey = url.substring(url.lastIndexOf("/")+1);
+		String gcmSenderId = req.getParameter("gcmSenderId");
+		String gcmSenderSecret = req.getParameter("gcmSenderSecret");
+		String apnSenderId = req.getParameter("apnSenderId");
+		String apnSenderSecret = req.getParameter("apnSenderSecret");
+		String wnsSenderId = req.getParameter("wnsSenderId");
+		String wnsSenderSecret = req.getParameter("wnsSenderSecret");
+   		
+		DatabaseHandle db = new DatabaseHandle();
+		
+		App app = db.getApp(appKey);
+		
+		app.setApnSenderSecret(apnSenderSecret);
+		app.setApnSenderId(apnSenderId);
+		app.setGcmSenderId(gcmSenderId);
+		app.setGcmSenderSecret(gcmSenderSecret);
+		app.setWnsSenderId(wnsSenderId);
+		app.setWnsSenderSecret(wnsSenderSecret);		
+		
+		db.updateApp(app);
+		
+   		resp.getWriter().println(req.getParameter(app.getJSONString()));
+    	}
 	
 	public void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException{
     	
